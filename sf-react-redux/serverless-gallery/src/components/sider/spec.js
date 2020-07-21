54c6741d24bff07b817cd9b1174d8c7130714cfc
eonsubae <eonsubae@naver.com>
2020-07-21 10:47:52 +0900

    Building the front-end data CRUD layer

---
+++ sf-react-redux/serverless-gallery/src/components/sider/spec.js
@@ -0,0 +1,33 @@
+import AvatarSection from 'components/sider/avatar';
+import { localPropTypesMockData } from 'lib/prop-types';
+import { findByTestAttr, setUpComponent } from 'lib/spec-utils';
+
+/* mocking the initial state of the store */
+const initialState = {
+  authentication: {
+    credentials: localPropTypesMockData.credentials,
+  },
+};
+
+describe('Avatar Component', () => {
+  let component;
+  beforeEach(() => {
+    component = setUpComponent(AvatarSection, initialState);
+    //console.log(component.debug());
+  });
+
+  it('It should render a avatar component', () => {
+    const wrapper = findByTestAttr(component, 'avatar');
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('It should render a input field', () => {
+    const wrapper = findByTestAttr(component, 'upload-input');
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('It should render a popconfirm component', () => {
+    const wrapper = findByTestAttr(component, 'popup-confirm');
+    expect(wrapper.length).toBe(1);
+  });
+});
