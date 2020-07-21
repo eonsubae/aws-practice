8425be6ba794c124154006abdb0fe8c73856e75b
eonsubae <eonsubae@naver.com>
2020-07-21 10:02:29 +0900

    Connecting authentication all together

--- sf-react-redux/serverless-gallery/src/App.js
+++ sf-react-redux/serverless-gallery/src/App.js
@@ -1,34 +1,12 @@
 import React, { Component } from 'react';
 
-import {
-  SignIn,
-  SignUp,
-  ForgotPwd,
-  ConfirmAccount,
-} from 'components/authentication';
+import Layout from './components/layout';
 import './App.css';
 import 'antd/dist/antd.css';
 
 class App extends Component {
   render() {
-    let AuthComp = null;
-
-    switch (4) {
-      case 1:
-        AuthComp = SignIn;
-        break;
-      case 2:
-        AuthComp = SignUp;
-        break;
-      case 3:
-        AuthComp = ForgotPwd;
-        break;
-      case 4:
-        AuthComp = ConfirmAccount;
-        break;
-    }
-
-    return <AuthComp ownMessage="My Own message"></AuthComp>;
+    return <Layout />;
   }
 }
 
--- sf-react-redux/serverless-gallery/src/reducers/index.js
+++ sf-react-redux/serverless-gallery/src/reducers/index.js
@@ -1,5 +1,6 @@
 /* local imports (all our reducers) */
 import authentication from './authentication';
+import global from './global';
 
 /* export reducers from a single entry point */
-export { authentication };
+export { authentication, global };
